dc31ad5f9275352f51d42afdab6a1111dfe0fd28
Martyna <martyna.doziwska@gmail.com>
2022-11-08 22:06:04 +0100

    installation

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,6 +1,6 @@
 ///<reference types="cypress" />
 
-Cypress.Comannds.add('enterTVN24.pl', () => {
+Cypress.Commands.add('enterTVN24.pl', () => {
 
     cy.visit('www.tvn24.pl')
     cy.get('#onetrust-accept-btn-handler')
